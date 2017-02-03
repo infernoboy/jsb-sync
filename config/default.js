59830a43223cefbfb7c4eeb45459730f29e1002a
Travis Roman <ffx1033@gmail.com>
2017-02-03 12:14:32 -0500

    Remove cookie secret, create config directory

---
+++ config/default.js
@@ -0,0 +1,11 @@
+/*
+* @Last modified in Sublime on Feb 03, 2017 12:12:31 PM
+*/
+
+// Rename or copy this file to index.js
+
+'use strict';
+
+module.exports = {
+	cookieSecret: 'ENTER A UNIQUE STRING'
+};
