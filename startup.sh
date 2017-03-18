5c8164efbb0e333e08a2dca45cb660ab5f64f2b6
Travis Roman <ffx1033@gmail.com>
2017-03-18 02:17:18 -0400

    Fixes

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ echo "$CONFIG_SSMTP" > /etc/ssmtp/ssmtp.conf
 echo "$CONFIG_REVALIASES" > /etc/ssmtp/revaliases
 
 chmod 644 /etc/ssmtp/ssmtp.conf
+chown nodejs:nodejs /home/nodejs/jsb-sync-data
 
 CMD="$@"
 
